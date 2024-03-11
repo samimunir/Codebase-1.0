8fad102f50378da5b5471ac6d881f11683f56538
Sami Munir <smunir01@outlook.com>
2024-03-11 15:42:49 +0000

    adding script.js & global variables to necessary HTML elements

---
+++ My Computer Science Projects/gamified-todo-list/src/script.js
@@ -0,0 +1,15 @@
+/*
+    Get references to:
+    - todo list container
+    - todo input field
+    - add todo button
+    - clear all todos button
+    - progress bar
+    - progress percentage
+*/
+const todo_list_container = document.getElementById("todo-list-container");
+const todo_input = document.getElementById("todo-input");
+const add_button = document.getElementById("add-todo-button");
+const clear_all_todos_button = document.getElementById("clear-all-todos-button");
+const progress_bar = document.getElementById("progress-bar");
+const progress_percentage = document.getElementById("progress-percentage");
