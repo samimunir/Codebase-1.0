47b0e40fe5ba5c3f772406741e4606118140df78
Sami Munir <smunir01@outlook.com>
2024-03-11 15:45:35 +0000

    adding function get_formatted_timestamp()

--- My Computer Science Projects/gamified-todo-list/src/script.js
+++ My Computer Science Projects/gamified-todo-list/src/script.js
@@ -13,3 +13,15 @@ const add_button = document.getElementById("add-todo-button");
 const clear_all_todos_button = document.getElementById("clear-all-todos-button");
 const progress_bar = document.getElementById("progress-bar");
 const progress_percentage = document.getElementById("progress-percentage");
+
+/*
+    function get_formatted_timestamp()
+    - returns formatted string of current time
+*/
+function get_formatted_timestamp() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString.padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
+}
