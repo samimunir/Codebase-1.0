55e92e23ecfa73fe4c5667712161d62356087922
Sami Munir <smunir01@outlook.com>
2024-03-13 10:33:53 +0000

    creating singly-linked-list object + global vars

---
+++ Data Structures and Algorithms/singly-linked-list/singly-linked-list/src/LinkedList.java
@@ -0,0 +1,9 @@
+public class LinkedList {
+    private Node head;
+    private int numberOfElements = 0;
+    private int pointer = -1;
+
+    public LinkedList() {
+        System.out.println("singly-linked-list initialization successful.");
+    }
+}
