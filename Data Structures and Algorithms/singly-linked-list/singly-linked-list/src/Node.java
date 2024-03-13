d786a1428f8cc97934fbeb48e8df958b927092d3
Sami Munir <smunir01@outlook.com>
2024-03-13 10:24:29 +0000

    adding linked list node object

---
+++ Data Structures and Algorithms/singly-linked-list/singly-linked-list/src/Node.java
@@ -0,0 +1,32 @@
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node() {}
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+}
