607f4d277209ff2dc1807e1c731a456cb504f118
Sami Munir <smunir01@outlook.com>
2024-03-14 07:17:01 +0000

    writing & testing function insert_at_head()

--- Data Structures and Algorithms/doubly-linked-list/doubly-linked-list/src/doubly_linked_list.c
+++ Data Structures and Algorithms/doubly-linked-list/doubly-linked-list/src/doubly_linked_list.c
@@ -42,16 +42,57 @@ void print_data_structure_stats() {
     printf("\tpointer: %d\n", pointer);
 }
 
+void insert_at_head(int data) {
+    printf("\ninsert_at_head(%d) called...\n", data);
+    if (is_empty()) {
+        head = (struct Node*) malloc(sizeof(struct Node));
+        head -> data = data;
+        head -> next = NULL;
+        head -> prev = NULL;
+        tail = head;
+        number_of_elements++;
+        pointer++;
+    } else {
+        if (head -> next == NULL) {
+            struct Node *new_head_node = (struct Node*) malloc(sizeof(struct Node));
+            new_head_node -> data = data;
+            new_head_node -> next = head;
+            new_head_node -> prev = NULL;
+            head = new_head_node;
+            head -> prev = new_head_node;
+            tail = tail -> next;
+            number_of_elements++;
+            pointer++;
+        } else {
+            struct Node *new_head_node = (struct Node*) malloc(sizeof(struct Node));
+            new_head_node -> data = data;
+            new_head_node -> next = head;
+            new_head_node -> prev = NULL;
+            head = new_head_node;
+            head -> prev = new_head_node;
+            number_of_elements++;
+            pointer++;
+        }
+    }
+    print_data_structure_stats();
+}
+
 int main(int argc, char* argv[]) {
     printf("doubly-linked-list\n------------------\n");
     /*
         Testing function printLinkedList()
     */
-    print_linked_list();
+    // print_linked_list();
     /*
         Testing function print_data_structure_stats()
     */
     print_data_structure_stats();
+    /*
+        Testing function insert_at_head()
+    */
+    insert_at_head(2);
+    insert_at_head(11);
+    insert_at_head(7);
 
     return EXIT_SUCCESS;
 }
