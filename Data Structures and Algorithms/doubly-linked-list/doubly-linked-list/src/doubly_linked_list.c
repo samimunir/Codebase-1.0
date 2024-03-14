7b954a232f8fbd5b471edc500896cee9bbb35720
Sami Munir <smunir01@outlook.com>
2024-03-14 05:23:56 +0000

    adding data structure: doubly-linked-list (C implementation)

---
+++ Data Structures and Algorithms/doubly-linked-list/doubly-linked-list/src/doubly_linked_list.c
@@ -0,0 +1,12 @@
+/*
+    doubly-linked-list data structure & algorithms
+    - doubly_linked_list.c
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[]) {
+    printf("doubly-linked-list\n------------------\n");
+    return EXIT_SUCCESS;
+}
