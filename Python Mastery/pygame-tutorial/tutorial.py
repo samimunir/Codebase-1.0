9a9d02ebf3cbb9a2b16a9a8515e23b46f3ae8875
Sami Munir <smunir01@outlook.com>
2024-03-19 09:42:48 -0400

    adding jumping and boundaries logic

--- Python Mastery/pygame-tutorial/tutorial.py
+++ Python Mastery/pygame-tutorial/tutorial.py
@@ -7,10 +7,12 @@ WINDOW = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("pygame tutorial")
 # Setting up position for character (top-left of window is (0, 0) --> (0 + x, 0 + y))
 x = 50
-y = 50
+y = 450
 width = 40
 height = 60
 velocity = 5
+isJump = False
+jumpCount = 10
 # Main game loop to check for collisions, input events, etc.
 RUNNING = True
 while RUNNING:
@@ -19,14 +21,27 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
     KEYS = pygame.key.get_pressed() # list of pygame key events
-    if KEYS[pygame.K_LEFT]:
+    if KEYS[pygame.K_LEFT] and x > velocity:
         x -= velocity
-    if KEYS[pygame.K_RIGHT]:
+    if KEYS[pygame.K_RIGHT] and x + width < 500:
         x += velocity
-    if KEYS[pygame.K_UP]:
-        y -= velocity
-    if KEYS[pygame.K_DOWN]:
-        y += velocity
+    if not(isJump):
+        if KEYS[pygame.K_UP] and y > velocity:
+            y -= velocity
+        if KEYS[pygame.K_DOWN] and y + height < 500:
+            y += velocity
+        if KEYS[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
     WINDOW.fill((0, 0, 0))
     pygame.draw.rect(WINDOW, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
