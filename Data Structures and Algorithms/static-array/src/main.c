f52dd8a46ef86a7b034c3ca5464827cec3ffad83
Sami Munir <smunir01@outlook.com>
2024-04-15 08:07:54 -0400

    adding data structure: static-array

---
+++ Data Structures and Algorithms/static-array/src/main.c
@@ -0,0 +1,17 @@
+/*
+    Sami Munir
+    April 15th, 2024
+    Codebase 1.0
+        Data Structures & Algorithms
+            Static Arrays
+            - main.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("static-array (implementation in C)\n");
+
+    return EXIT_SUCCESS;
+}
