3001c49cf06d18e144e082acc19a1ae8c40ea9fe
Sami Munir <smunir01@outlook.com>
2024-04-23 11:26:11 -0400

    updating function initialize_linked_list() & adding util functions for printing

--- Data Structures and Algorithms/singly-linked-list/src/singly_linked_list.c
+++ Data Structures and Algorithms/singly-linked-list/src/singly_linked_list.c
@@ -5,13 +5,13 @@
 
 void print_linked_list(struct linked_list_node *head) {
     if (head == NULL) {
-        printf("\tHEAD is either NULL or not initialized.\n");
+        printf("HEAD is either NULL or not initialized.\n");
         return;
     } else if (head -> next == NULL) {
-        printf("\tHEAD -> %d -> NULL\n", head -> data);
+        printf("HEAD -> %d -> NULL\n", head -> data);
         return;
     } else {
-        printf("\tHEAD -> ");
+        printf("HEAD -> ");
         struct linked_list_node *current_node = head;
         while (current_node != NULL) {
             printf("%d -> ", current_node -> data);
@@ -22,7 +22,21 @@ void print_linked_list(struct linked_list_node *head) {
     }
 }
 
-void print_linked_list_struct(struct singly_linked_list *linked_list) {}
+void print_linked_list_struct(struct singly_linked_list *linked_list) {
+    if (linked_list == NULL) {
+        printf("linked_list is NULL/not initialized.\n");
+        return;
+    }
+    printf("\tlinked_list: ");
+    print_linked_list(linked_list -> head);
+    printf("\t# elements: %d\n", linked_list -> number_of_elements);
+    printf("\tbytes/(1) node: %d bytes\n", linked_list -> linked_list_node_memory);
+    printf("\t\ttotal memory: %d bytes\n", linked_list -> memory);
+    printf("\t\tused memory: %d bytes\n", linked_list -> used_memory);
+    printf("\t\tavail memory: %d bytes\n", linked_list -> available_memory);
+    printf("\tpayload: %.2f\n", linked_list -> payload);
+    return;
+}
 
 struct singly_linked_list * initialize_linked_list() {
     printf("\ninitialize_linked_list() called -->\n");
@@ -38,13 +52,15 @@ struct singly_linked_list * initialize_linked_list() {
         return NULL;
     }
     linked_list -> head = head;
+    linked_list -> linked_list_node_memory = sizeof(struct linked_list_node);
     linked_list -> number_of_elements = 0;
-    linked_list -> memory = 0;
-    linked_list -> available_memory = 0;
-    linked_list -> used_memory = 0;
+    linked_list -> memory = MAX_MEMORY;
+    linked_list -> used_memory = linked_list -> number_of_elements * linked_list -> linked_list_node_memory;
+    linked_list -> available_memory = linked_list -> memory - linked_list -> used_memory;
     linked_list -> payload = 0.0;
 
-    printf("\tlinked list successfully initialzed.\n");
+    printf("\tlinked_list successfully initialzed.\n");
+    print_linked_list_struct(linked_list);
     
     return linked_list;
 }
--- Data Structures and Algorithms/singly-linked-list/src/singly_linked_list.h
+++ Data Structures and Algorithms/singly-linked-list/src/singly_linked_list.h
@@ -3,9 +3,12 @@ struct linked_list_node {
     struct linked_list_node *next;
 };
 
+#define MAX_MEMORY 4096
+
 struct singly_linked_list {
     struct linked_list_node *head;
     int number_of_elements;
+    int linked_list_node_memory;
     int memory;
     int used_memory;
     int available_memory;
